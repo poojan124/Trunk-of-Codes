a924b4a86d68561eabaa299fc3ae781712cae06c
poojancs88 <poojancs88@gmail.com>
2016-04-19 18:13:17 +0530

    Not done!

---
+++ hacker_rank_matrix_rotation.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<iostream>
+#include<math.h>
+#include<algorithm>
+#include<vector>
+using namespace std;
+template<class t> t _min(t a,t b){return a<b?a:b;}
+template<class t> t _min(t a,t b,t c){return (a<b?(a<c?a:c):(b<c?b:c));}
+template<class t> t _max(t a,t b){return a>b?a:b;}
+template<class t> t _max(t a,t b,t c){return (a>b?(a>c?a:c):(b>c?b:c));}
+int main()
+{
+    long int arr[400][400],ans[400][400],r,m,n;
+    scanf("%ld %ld %ld",&m,&n,&r);
+    r=r%(2*(m+n-2));
+    for(long int i=0;i<m;i++)
+    {
+        for(long int j=0;j<n;j++)
+        {
+
+        }
+    }
+    return 0;
+}
