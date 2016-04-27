46576d68ecfa9a0886815aeca7eaacd07d4314cc
poojancs88 <poojancs88@gmail.com>
2016-04-27 11:08:12 +0530

    Done Yeah!

--- spoj_ACPC10B.cpp
+++ spoj_ACPC10B.cpp
@@ -1,20 +1,37 @@
 #include<stdio.h>
 #include<iostream>
-#include<math.h>
+#include<cmath>
 #include<unordered_map>
 using namespace std;
+int ar[]={145,42,20,4,16,37,58,89};
+int min(int a,int b,int c){
+return a<b?(a<c?a:c):(b<c?b:c);
+}
+bool is_in(int n){
+    for(int i=0;i<8;i++)
+        if(n==ar[i])
+            return true;
+
+    return false;
+}
+int get_idx(int n){
+    for(int i=0;i<8;i++)
+        if(n==ar[i])
+            return i;
+
+}
 int main()
 {
     int a,b;
     scanf("%d %d",&a,&b);
     while(a!=0&&b!=0){
-        int count=1,ar[]={145,42,20,4,16,37,58},seq1;
+        int count=1,seq1;
         int temp1=a,flag=0,temp2=b,digit,sum=0;
         unordered_map<int,int> m1,m2;
         m1[temp1]=count;
         m2[temp2]=count;
         while(1){
-            if(temp1==89||temp1==1)
+            if(is_in(temp1)==true||temp1==1)
                 break;
             sum=0;
             while(temp1>0)
@@ -41,12 +58,14 @@ int main()
             cout<<it->first<<" "<<it->second<<endl;
         }**/
         if(m1.find(temp2)!=m1.end()){
-            cout<<1+m1[temp2]<<endl;
+            cout<<a<<" "<<b<<" "<<1+m1[temp2]<<endl;
         }
         else{
             count=1;
             while(1){
                 sum=0;
+                if(is_in(temp2)==true||temp2==1)
+                    break;
                 while(temp2>0)
                 {
                     digit=temp2%10;
@@ -64,29 +83,22 @@ int main()
                         break;
                 }
                 //cout<<temp2<<endl;
-                if(temp2==89||temp2==1)
-                    break;
+
             }
-            //cout<<"temp2 "<<temp2<<" temp1 "<<temp1<<endl;
+            //cout<<"temp2 "<<temp2<<" temp1 "<<temp1<<" m[temp1] : "<<m1[temp1]<<" m[temp2] : "<<m2[temp2]<<endl;
             if(flag==1){
-                cout<<seq1+count<<endl;
+                cout<<a<<" "<<b<<" "<<seq1+count<<endl;
             }
             if(flag==0){
                 int idx=0;
                 if(temp2==1||temp1==1)
-                    cout<<"0"<<endl;
-                else if(temp2==89){
-                    while(idx<7){
-                        if((m1.find(ar[idx])!=m1.end())&&(m1.find(ar[idx])!=m1.end()))
-                        {
-                            cout<<m1[ar[idx]]+m2[ar[idx]]<<endl;
-                            break;
-                        }
-                        idx++;
-                    }
-
+                    cout<<a<<" "<<b<<" "<<"0"<<endl;
+                else{
+                    int idx1,idx2;
+                    idx1=get_idx(temp1);
+                    idx2=get_idx(temp2);
+                    cout<<a<<" "<<b<<" "<<m1[temp1]+m2[temp2]+min(abs(idx1-idx2),7-idx1+idx2+1,7-idx2+idx1+1)<<endl;
                 }
-
             }
         }
         scanf("%d %d",&a,&b);
