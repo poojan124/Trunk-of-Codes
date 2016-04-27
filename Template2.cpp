c262d689f01064aedc868c16659050158070f6e5
poojancs88 <poojancs88@gmail.com>
2016-04-27 07:50:45 +0530

    Version 2

--- Template2.cpp
+++ Template2.cpp
@@ -3,12 +3,19 @@
 #include<math.h>
 #include<algorithm>
 #include<vector>
+#include<unordered_map>
+#include<map>
+#include<limits.h>
+#define ll long long
+#define mod (int)(1e+9+7)
 using namespace std;
-template<class t> t _min(t a,t b){return a<b?a:b;}
-template<class t> t _min(t a,t b,t c){return (a<b?(a<c?a:c):(b<c?b:c));}
-template<class t> t _max(t a,t b){return a>b?a:b;}
-template<class t> t _max(t a,t b,t c){return (a>b?(a>c?a:c):(b>c?b:c));}
-template<class t,class u>void arr_reader(t *arr,u n){for(u i=0;i<n;i++)cin>>arr[i];};
+template<class t> t min(t a,t b){return a<b?a:b;}
+template<class t> t min(t a,t b,t c){return (a<b?(a<c?a:c):(b<c?b:c));}
+template<class t> t max(t a,t b){return a>b?a:b;}
+template<class t> t max(t a,t b,t c){return (a>b?(a>c?a:c):(b>c?b:c));}
+template<class t> void read(t *a,int n){for(int i=0;i<n;i++)cin>>a[i];}
+template<class t> void dis_n(t *a,int n){for(int i=0;i<n;i++)cout<<a[i]<<endl;}
+template<class t> void dis_s(t *a,int n){for(int i=0;i<n;i++)cout<<a[i]<<" ";cout<<endl;}
 int main()
 {
 
